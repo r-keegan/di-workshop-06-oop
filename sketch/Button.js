120223023039bca3a538eb4ca8c529e97b7e92f9
Rebecca Keegan <rebeccachristinekeegan@gmail.com>
2019-04-09 14:44:50 +0100

    add basic sketch with button class

---
+++ sketch/Button.js
@@ -0,0 +1,13 @@
+class Button {
+    constructor(label, x, y) {
+        console.log('label: ', label)
+        console.log('x: ', x)
+        console.log('y: ', y)
+
+        this.label = label;
+        this.x = x;
+        this.y = y;
+
+        console.log(this)
+    }
+}
---
+++ sketch/sketch.js
@@ -0,0 +1,10 @@
+var dogButton = new Button('Dog', 10, 10);
+
+function setup() {
+    createCanvas(400,400)
+
+}
+
+function draw() {
+    background(220);
+}

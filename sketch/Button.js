e7a4eca19edd21b4a76afb43eec148022cc82d62
Rebecca Keegan <rebeccachristinekeegan@gmail.com>
2019-04-09 15:17:03 +0100

    Added more buttons, made button change colour when hover

--- sketch/Button.js
+++ sketch/Button.js
@@ -1,13 +1,36 @@
 class Button {
     constructor(label, x, y) {
-        console.log('label: ', label)
-        console.log('x: ', x)
-        console.log('y: ', y)
-
         this.label = label;
         this.x = x;
         this.y = y;
+    }
 
-        console.log(this)
+    draw() {
+        if (this.containsMouse()) {
+            fill('lightskyblue')
+        } else {
+            fill(250)
+        }
+        rect(this.x, this.y, 100, 30);
+
+        fill(0)
+        textAlign(CENTER)
+        text(this.label, this.x + 50, this.y + 20)
+    }
+
+    containsPoint(x,y) {
+        var left = this.x;
+        var right = this.x + 100;
+        var top = this.y;
+        var bottom = this.y + 30;
+
+        var isInHorizontalRange = (left < x) && (x < right);
+        var isInVerticalRange = (top < y) && (y < bottom);
+
+        return isInHorizontalRange && isInVerticalRange
+    }
+
+    containsMouse() {
+        return this.containsPoint(mouseX, mouseY)
     }
 }
--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,4 +1,8 @@
-var dogButton = new Button('Dog', 10, 10);
+var dogButton = new Button('DOG', 10, 10);
+var catButton = new Button('CAT', 120, 10);
+var cowButton = new Button('COW', 10, 50);
+var pizzaButton = new Button('PIZZA', 120, 50)
+
 
 function setup() {
     createCanvas(400,400)
@@ -7,4 +11,24 @@ function setup() {
 
 function draw() {
     background(220);
+
+    dogButton.draw()
+    catButton.draw()
+    cowButton.draw()
+    pizzaButton.draw()
+}
+
+function mousePressed() {
+    if (dogButton.containsMouse()) {
+        alert("Woof")
+    }
+    if (catButton.containsMouse()) {
+        alert("Meow")
+    }
+    if (cowButton.containsMouse()) {
+        alert("Moo")
+    }
+    if (pizzaButton.containsMouse()) {
+        alert("YES PLEASE")
+    }
 }
